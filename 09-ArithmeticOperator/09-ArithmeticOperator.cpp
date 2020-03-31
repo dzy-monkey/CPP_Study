1ec075029796603e262d542834db08835f4bbd0b
dzy-monkey <362401513@qq.com>
2020-04-01 00:50:46 +0800

    更新了自述文件，添加了运算符模块学习内容

--- 09-ArithmeticOperator/09-ArithmeticOperator.cpp
+++ 09-ArithmeticOperator/09-ArithmeticOperator.cpp
@@ -2,10 +2,63 @@
 算数运算符
 */
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    //+ - * /
+#pragma region 整型的运算
+	//+ - * /
+	/*
+	int a;
+	cout << "请输入第一个数字" << endl;
+	cin >> a;
+	cout << "你输入的第一个数字是：" << a << endl;
+	int b;
+	cout << "请输入第二个数字" << endl;
+	cin >> b;
+	cout << "你输入的第二个数字是：" << b << endl;
+	int res1;
+	int res2;
+	int res3;
+	int res4;
+
+	res1 = a + b;
+	res2 = a - b;
+	res3 = a * b;
+	res4 = a / b;
+
+	cout << "两个数字的和是：" << res1 << endl;
+	cout << "两个数字的差是：" << res2 << endl;
+	cout << "两个数字的积是：" << res3 << endl;
+	cout << "两个数字的商是：" << res4 << endl;
+	*/
+#pragma endregion
+
+#pragma region 浮点型的运算
+	float a;
+	cout << "请输入第一个数字" << endl;
+	cin >> a;
+	cout << "你输入的第一个数字是：" << a << endl;
+	float b;
+	cout << "请输入第二个数字" << endl;
+	cin >> b;
+	cout << "你输入的第二个数字是：" << b << endl;
+	float res1;
+	float res2;
+	float res3;
+	float res4;
+
+	res1 = a + b;
+	res2 = a - b;
+	res3 = a * b;
+	res4 = a / b;
+
+	cout << "两个数字的和是：" << res1 << endl;
+	cout << "两个数字的差是：" << res2 << endl;
+	cout << "两个数字的积是：" << res3 << endl;
+	cout << "两个数字的商是：" << res4 << endl;
+#pragma endregion
+
 
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# CPP_Study
+# C++学习笔记项目
+	
+## 01-HelloCPP
+- 搭建好C++运行环境，输出HelloCPP 
+
+## 02-UserInput
+- 用户输入的方法运用
+
+## 03-Variate
+- C++中变量的命名规范
+
+## 04-Integer
+- 整型
+
+## 05-CharacterTypes
+- 字符类型
+
+## 06-BoolTypes
+- 布尔类型
+
+## 07-Constant
+- 常量
+
+## 08-Floating_Point
+- 浮点型
+
+## 09-ArithmeticOperator
+- 算数运算符
